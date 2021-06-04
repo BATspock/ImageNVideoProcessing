6a8cb553d5c8c54894f151ce5e27710f201b8ea7
Aditya Kishore <30798316+BATspock@users.noreply.github.com>
2021-06-04 16:48:36 +0530

    rotate image with getMatrixRotation2D and wrapAffine

--- basic_operations/rotateby45and90.py
+++ basic_operations/rotateby45and90.py
@@ -6,10 +6,18 @@ img_pth = "C://Users//adkishor//Desktop//ImageNVideoProcessing//misc//"
 
 rot = input("Enter the angle by which to rotate the image in degrees: ")
 
-rot = float(rot)
-for img in os.listdir(img_pth)[:2]:
+#rot = float(rot)
+#for img in os.listdir(img_pth):
+#    im = cv2.imread(img_pth+img, 0)
+#    rot = imutils.rotate(im, 45)
+#    cv2.imshow("check ", rot)
+#    cv2.waitKey(0)
+#    cv2.destroyAllWindows()  
+
+for img in os.listdir(img_pth)[:4]:
     im = cv2.imread(img_pth+img, 0)
-    rot = imutils.rotate(im, rot)
-    cv2.imshow("check ", rot)
+    M = cv2.getRotationMatrix2D((im.shape[0]//2, im.shape[1]//2), float(rot), 1)
+    i = cv2.warpAffine(im, M, (im.shape[1], im.shape[0]))
+    cv2.imshow("rotation", i)
     cv2.waitKey(0)
-    cv2.destroyAllWindows()  
+    cv2.destroyAllWindows() 
