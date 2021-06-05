a57aea28dc5fda48683f891e6d82944859c75fd8
Aditya Kishore <30798316+BATspock@users.noreply.github.com>
2021-06-05 16:53:02 +0530

    removed imutils rotate

--- basic_operations/rotateby45and90.py
+++ basic_operations/rotateby45and90.py
@@ -14,7 +14,7 @@ rot = input("Enter the angle by which to rotate the image in degrees: ")
 #    cv2.waitKey(0)
 #    cv2.destroyAllWindows()  
 
-for img in os.listdir(img_pth)[:4]:
+for img in os.listdir(img_pth):
     im = cv2.imread(img_pth+img, 0)
     M = cv2.getRotationMatrix2D((im.shape[0]//2, im.shape[1]//2), float(rot), 1)
     i = cv2.warpAffine(im, M, (im.shape[1], im.shape[0]))
