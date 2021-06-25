0cda59468f627411d294290136b85e8606fcf9a7
Aditya Kishore <30798316+BATspock@users.noreply.github.com>
2021-06-25 22:12:58 +0530

    complete jpeg implemented for images

--- jpegImplementation/createjpeg.py
+++ jpegImplementation/createjpeg.py
@@ -3,6 +3,15 @@ import os
 import numpy as np
 from scipy.fftpack import dct, idct
 
+QUANTIZATION_MATRIX = np.array([[16,11, 10, 16,24,40,51,61],
+                                  [12,12,14,19,26,58,60,55],
+                                  [14,13,16,24,40,57,69,56],
+                                  [14,17,22,29,51,87,80,62],
+                                  [18,22,37,56,68,109,103,77],
+                                  [24,35,55,64,81,104,113,92],
+                                  [49,64,78,87,103,121,120,101],
+                                  [72,92,95,98,112,100,103,99]])
+
 ENV_SHAPE = 8
 
 def addBorderReplicateforJPEG8X8(im: np.ndarray) -> np.ndarray:
@@ -40,8 +49,9 @@ def applyDCTonImage(im, ENV_SHAPE= 8) -> np.ndarray:
     for r in range(0,im.shape[0], ENV_SHAPE):
         for c in range(0,im.shape[1], ENV_SHAPE):
             
-            temp_block = im[r:r+ENV_SHAPE, c: c+ENV_SHAPE] 
-            dct_temp_block = apply_dct(temp_block)
+            temp_block = im[r:r+ENV_SHAPE, c: c+ENV_SHAPE] - 128
+            dct_temp_block = apply_dct(np.int64(temp_block))
+            dct_temp_block = qunatize_dct_8x8(dct_temp_block)
             test_image[r:r+ENV_SHAPE, c: c+ENV_SHAPE] = dct_temp_block
     return test_image
 
@@ -54,27 +64,40 @@ def applyIDCTonImage(im, ENV_SHAPE= 8) -> np.ndarray:
         for c in range(0,im.shape[1], ENV_SHAPE):
             
             temp_block = im[r:r+ENV_SHAPE, c: c+ENV_SHAPE] 
+            temp_block = return_qunatize_8x8_for_idct(temp_block)
             idct_temp_block = apply_idct2(temp_block)
-            test_image[r:r+ENV_SHAPE, c: c+ENV_SHAPE] = idct_temp_block 
+            test_image[r:r+ENV_SHAPE, c: c+ENV_SHAPE] = idct_temp_block + 128
     return test_image
+
+def qunatize_dct_8x8(im : np.ndarray,  q = QUANTIZATION_MATRIX) -> np.ndarray:
+    return np.int64(np.true_divide(im, q))
+
+
+def return_qunatize_8x8_for_idct(im :np.ndarray, q = QUANTIZATION_MATRIX) -> np.ndarray:
+    return np.multiply(im,q)
+
 
 
 img_pth = "C://Users//adkishor//Desktop//ImageNVideoProcessing//misc//"
 
-for img in os.listdir(img_pth)[:1]:
+for img in os.listdir(img_pth)[:]:
     
     im = addBorderReplicateforJPEG8X8(cv2.imread(img_pth+img,0))
     #print(im.dtype)
     cv2.imshow("check", im)  
     #im = cv2.cvtColor(im, cv2.COLOR_BGR2YCR_CB)  
+    #print(im[0:8,0:8])
     get_DCT_image = applyDCTonImage(im.copy())    
     #print(get_DCT_image.dtype)
+    #print(get_DCT_image[0:8,0:8])
     cv2.imshow("check_DCT", get_DCT_image)
     get_IDCT_image = applyIDCTonImage(get_DCT_image.copy())
     #get_IDCT_image = np.asarray(get_IDCT_image, dtype=np.float64)
     #cv2.imshow("check_idct", get_IDCT_image)
     #print(get_IDCT_image.dtype)
+    #print(get_IDCT_image[0:8,0:8])
     get_IDCT_image = np.uint8(get_IDCT_image)
+    #print(get_IDCT_image[0:8,0:8])
     cv2.imshow("check_idct_uint8", get_IDCT_image)
     #print(np.allclose(get_IDCT_image,im) )
     
