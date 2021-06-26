d7bb98dcb02a3e0aa0e64df6041a3771d20ea3f8
Aditya Kishore <30798316+BATspock@users.noreply.github.com>
2021-06-26 22:21:30 +0530

    check MSE loss

--- jpegImplementation/predictionError.py
+++ jpegImplementation/predictionError.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+import numpy as np
 from createjpeg import return_compressed_image
 
 img_pth = "C://Users//adkishor//Desktop//ImageNVideoProcessing//misc//"
@@ -8,12 +9,13 @@ img_pth = "C://Users//adkishor//Desktop//ImageNVideoProcessing//misc//"
 #cv2.imshow('image', i)
 #cv2.waitKey(0)
 #cv2.destroyAllWindows()
-for im in os.listdir(img_pth):
+for im in os.listdir(img_pth)[:3]:
     
     i = cv2.imread(img_pth+str(im),0)
-    cv2.imshow("original image", i )
+    #cv2.imshow("original image", i )
     dct, compressed_image = return_compressed_image(img_pth+str(im))
-    cv2.imshow("DCT", dct)
-    cv2.imshow("compressed image",compressed_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    #cv2.imshow("DCT", dct)
+    #cv2.imshow("compressed image",compressed_image)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+    print("avg MSE loss: ",(np.mean((i-compressed_image)**2))**(1/2))
