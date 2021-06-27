4003754fda7c1b469f260bc905b5577846ccf7a6
Aditya Kishore <30798316+BATspock@users.noreply.github.com>
2021-06-27 20:49:59 +0530

    added histogram check in jupyter notebook

---
+++ jpegImplementation/hist.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+import os
+
+def create_histogram(im :np.ndarray, channel = 0, mask = None)->np.ndarray:
+    
+    return cv2.calcHist([im],[channel],mask,[256],[0,256])
+
+
+img_pth = "C://Users//adkishor//Desktop//ImageNVideoProcessing//misc//"
+
+#i = cv2.imread(img_pth+ '4.1.01.tiff', 0)
+#cv2.imshow('image', i)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+for im in os.listdir(img_pth)[:3]:
+    i = cv2.imread(img_pth+str(im), 0)
+    hist = create_histogram(i)
+    plt.plot(hist)
+    plt.show()
+
