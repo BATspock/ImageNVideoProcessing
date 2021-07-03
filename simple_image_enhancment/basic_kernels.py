497ee0d40976925d2236e48b8dfa3db6c52dbe3c
BATspock <30798316+BATspock@users.noreply.github.com>
2021-07-03 23:19:14 +0530

    added basic image enhancement kernels

--- simple_image_enhancment/basic_kernels.py
+++ simple_image_enhancment/basic_kernels.py
@@ -7,20 +7,18 @@ print(os.getcwd())
 
 img_pth = str(os.getcwd())+"//misc//"
 
-print(img_pth)
+#print(img_pth)
 
-'''
 
 for im in os.listdir(img_pth)[:3]:
 
-    i = cv2.imread(img_pth+str(im),0)
-    hi = cv2.equlaizeHist(i)
+    i = cv2.imread(img_pth+str(im),0)   
+    hi = cv2.equalizeHist(i)
     avg = cv2.blur(i,(8,8))
-    med = cv2.medianBlur(i, 8)
+    med = cv2.medianBlur(i, 9)
+    cv2.imshow("Origina image", i)
     cv2.imshow("histogram equlization", hi)
     cv2.imshow("averaging", avg)
     cv2.imshow("median blur", med)
     cv2.waitKey(0)
-    cv2.destoryAllWindows()
-
-'''
+    cv2.destroyAllWindows()
