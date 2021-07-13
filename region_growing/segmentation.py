fb6fdfd104b873b8d675ab2ff0f7db9dbfa5713c
BATspock <30798316+BATspock@users.noreply.github.com>
2021-07-13 13:05:57 +0530

    graph cut for point input

--- region_growing/segmentation.py
+++ region_growing/segmentation.py
@@ -10,29 +10,35 @@ os.chdir("..")
 
 img_pth = str(os.getcwd())+"//misc//"
 
-mouse_clicks=list()
 #print(img_pth)
 def click_event(event, x, y, flags, params):
+    global mouseX, mouseY
     if event == cv2.EVENT_LBUTTONDOWN:
-        print(x, " ", y)
+        print("foreground pixel location: ",x, " ", y)
 
-    mouse_clicks.append([x,y])
+    mouseX, mouseY = x, y
 
 def foreground_create(image: np.ndarray, x: int, y: int, threshold: float)->np.ndarray:
+    print("foreground create")
     for r in range(image.shape[0]):
         for c in range(image.shape[1]):
-            dis = create_fg(mouse_clicks[0][0], mouse_clicks[0][1], r, c, image)
+            dis = create_fg(mouseX, mouseY, r, c, image)
             if (dis<threshold):
                 image[r,c] = 255
 
     return image.copy()
 
 
-for im in os.listdir(img_pth)[:1]:
+image_list =  os.listdir(img_pth)
+im = cv2.imread(img_pth+str(image_list[0]),0)
+cv2.namedWindow("image")
+cv2.setMouseCallback("image", click_event)
+cv2.imshow("image", im)
 
-    i = cv2.imread(img_pth+str(im),0)
-    cv2.imshow("image", i)
-    cv2.setMouseCallback("image", click_event)
-    #add code to show separated image
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+
+
+cv2.waitKey(0)
+im_new = foreground_create(im, mouseX, mouseY, 5)
+cv2.imshow("new image", im_new)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- region_growing/sepration.py
+++ region_growing/sepration.py
@@ -8,19 +8,19 @@ def pixel_wt (px :int, py:int, image :np.ndarray)-> float:
     rows, cols = image.shape[0], image.shape[1]
     left, right, top, bottom, diag_bt, diag_top, o_dia_bt, o_dia_top = 0,0,0,0,0,0,0,0
 
-    if (px>=1): left = image[px, py]
-    if (px<= rows-2): right = image[px+1,py]
+    if (px>=1): left = int(image[px, py])
+    if (px<= rows-2): right = int(image[px+1,py])
 
-    if(py>=1): top = image[px, py-1]
-    if(py<=cols-2) : bottom = image[px, py+1]
+    if(py>=1): top = int(image[px, py-1])
+    if(py<=cols-2) : bottom = int(image[px, py+1])
 
-    if (px>=1 and py >=1): diag_top = image[px-1, py-1]
-    if (px<=rows-2 and py<=cols-2) : diag_bt = image[px+1, py+1]
+    if (px>=1 and py >=1): diag_top = int(image[px-1, py-1])
+    if (px<=rows-2 and py<=cols-2) : diag_bt = int(image[px+1, py+1])
 
-    if (px<=rows-2 and py >=1): o_dia_top = image[px+1, py-1]
-    if (px >=1 and py <=rows-2): o_dia_bt = image[px-1, py+1]
+    if (px<=rows-2 and py >=1): o_dia_top = int(image[px+1, py-1])
+    if (px >=1 and py <=rows-2): o_dia_bt = int(image[px-1, py+1])
 
-    return (left+right+top+bottom+diag_bt+diag_top+o_dia_bt+o_dia_top+(2*image[px,py]))/9.0
+    return (left+right+top+bottom+diag_bt+diag_top+o_dia_bt+o_dia_top+(2*image[px,py]))/9
 
 def distance(wt :float, px :int, py :int, image :np.ndarray)-> float:
     
