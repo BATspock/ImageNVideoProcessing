d9c7d5caedd105636ccfe4deb1de395656785bb6
BATspock <30798316+BATspock@users.noreply.github.com>
2021-08-03 23:08:07 +0530

    geodesic contours for level set

--- level_set/ForceEquation.py
+++ level_set/ForceEquation.py
@@ -1,1 +1,37 @@
 
+def curvature(f):
+    fy, fx = grad(f)
+    norm = np.sqrt(fx**2 + fy**2)
+    Nx = fx / (norm + 1e-8)
+    Ny = fy / (norm + 1e-8)
+    return div(Nx, Ny)
+
+
+def div(fx, fy):
+    fyy, fyx = grad(fy)
+    fxy, fxx = grad(fx)
+    return fxx + fyy
+
+
+def dot(x, y, axis=0):
+    return np.sum(x * y, axis=axis)
+
+
+v = 1.
+dt = 1.
+
+g = stopping_fun(img_smooth, alpha)
+dg = grad(g)
+
+for i in range(n_iter):
+    dphi = grad(phi)
+    dphi_norm = norm(dphi)
+    kappa = curvature(phi)
+
+    smoothing = g * kappa * dphi_norm
+    balloon = g * dphi_norm * v
+    attachment = dot(dphi, dg)
+
+    dphi_t = smoothing + balloon + attachment
+
+    phi = phi + dt * dphi_t
